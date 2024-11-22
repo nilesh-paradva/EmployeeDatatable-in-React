4f4cdbcea03aa63bb32fcce73bd6c91f72f873a9
nilesh7272 <nileshparadva97@gmail.com>
2024-11-22 17:33:59 +0530

    employee data table

--- src/components/employeeview/EmployeeView.jsx
+++ src/components/employeeview/EmployeeView.jsx
@@ -55,60 +55,62 @@ const EmployeeView = () => {
                                     </div>
                                 </div>
                                 <input type="text" placeholder="Search..." className="mt-3 px-3 py-2 border rounded-lg mb-3 outline-none" value={Search} onChange={handleSearch} />
-                                {filteredData.length === 0 ? (<p className="text-center">No employee available. Please add some employee.</p>) : (
-                                    <table className="table-auto w-full text-left border-collapse">
-                                        <thead>
-                                            <tr className="bg-blue-200 text-center">
-                                                <th className="border py-2">ID</th>
-                                                <th className="border py-2">
-                                                    <DropdownButton id="dropdown-basic-button" title={<span className="font-bold">Name <i className="fa-solid fa-arrows-up-down ps-2"></i></span>}>
-                                                        <Dropdown.Item href="#/action-1" className="!rounded-lg" onClick={() => handleSort('asc', 'name')}><i className="fa-solid fa-arrow-down-a-z"></i></Dropdown.Item>
-                                                        <Dropdown.Item href="#/action-2" className="!rounded-lg" onClick={() => handleSort('dsc', 'name')}><i className="fa-solid fa-arrow-up-z-a"></i></Dropdown.Item>
-                                                    </DropdownButton>
-                                                </th>
-                                                <th className="border px-4 py-2">Age</th>
-                                                <th className="border px-4 py-2">
-                                                    <DropdownButton id="dropdown-basic-button" title={<span className="font-bold">Department <i className="fa-solid fa-arrows-up-down ps-2"></i></span>} className="p-0 m-0">
-                                                        <Dropdown.Item href="#/action-1" className="text-center !rounded-lg" onClick={() => handleSort('asc', 'department')}><i className="fa-solid fa-arrow-down-a-z"></i></Dropdown.Item>
-                                                        <Dropdown.Item href="#/action-2" className="text-center !rounded-lg" onClick={() => handleSort('dsc', 'department')}><i className="fa-solid fa-arrow-up-z-a"></i></Dropdown.Item>
-                                                    </DropdownButton>
-                                                </th>
-                                                <th className="border px-4 py-2">
-                                                    <DropdownButton id="dropdown-basic-button" title={<span className="font-bold">Position <i className="fa-solid fa-arrows-up-down ps-2"></i></span>} className="p-0 m-0">
-                                                        <Dropdown.Item href="#/action-1" className="text-center !rounded-lg" onClick={() => handleSort('asc', 'position')}><i className="fa-solid fa-arrow-down-a-z"></i></Dropdown.Item>
-                                                        <Dropdown.Item href="#/action-2" className="text-center !rounded-lg" onClick={() => handleSort('dsc', 'position')}><i className="fa-solid fa-arrow-up-z-a"></i></Dropdown.Item>
-                                                    </DropdownButton>
-                                                </th>
-                                                <th className="border px-4 py-2">Salary</th>
-                                                <th className="border px-4 py-2">
-                                                    <DropdownButton id="dropdown-basic-button" title={<span className="font-bold">Email <i className="fa-solid fa-arrows-up-down ps-2"></i></span>} className="p-0 m-0">
-                                                        <Dropdown.Item href="#/action-1" className="text-center !rounded-lg" onClick={() => handleSort('asc', 'email')}><i className="fa-solid fa-arrow-down-a-z"></i></Dropdown.Item>
-                                                        <Dropdown.Item href="#/action-2" className="text-center !rounded-lg" onClick={() => handleSort('dsc', 'email')}><i className="fa-solid fa-arrow-up-z-a"></i></Dropdown.Item>
-                                                    </DropdownButton>
-                                                </th>
-                                                <th className="border px-4 py-2">Action</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {filteredData.map((emp) => (
-                                                <tr key={emp.id} className="hover:bg-gray-100 text-center">
-                                                    <td className="border px-4 py-3">{emp.id}</td>
-                                                    <td className="border px-4 py-3">{emp.name}</td>
-                                                    <td className="border px-4 py-3">{emp.age}</td>
-                                                    <td className="border px-4 py-3">{emp.department}</td>
-                                                    <td className="border px-4 py-3">{emp.position}</td>
-                                                    <td className="border px-4 py-3">{emp.salary}</td>
-                                                    <td className="border px-4 py-3">{emp.email}</td>
-                                                    <td className="border px-4 py-3">
-                                                        <button className="bg-green-700 p-2 px-3 rounded-lg me-2 text-white" onClick={() => empView(emp.id)}><i className="fa-solid fa-eye"></i></button>
-                                                        <button className="bg-blue-700 p-2 px-3 rounded-lg me-2 text-white" onClick={() => empEdit(emp.id)}><i className="fa-solid fa-pen-to-square"></i></button>
-                                                        <a href="#" className="bg-red-700 p-2 px-3 rounded-lg text-white" onClick={() => empDelete(emp.id)}><i className="fa-solid fa-trash"></i></a>
-                                                    </td>
+                                <div className="overflow-x-scroll">
+                                    {filteredData.length === 0 ? (<p className="text-center">No employee available. Please add some employee.</p>) : (
+                                        <table className="table-auto text-left border-collapse w-[1250px]">
+                                            <thead>
+                                                <tr className="bg-blue-200 text-center">
+                                                    <th className="border py-2">ID</th>
+                                                    <th className="border py-2">
+                                                        <DropdownButton id="dropdown-basic-button" title={<span className="font-bold">Name <i className="fa-solid fa-arrows-up-down ps-2"></i></span>}>
+                                                            <Dropdown.Item href="#/action-1" className="!rounded-lg" onClick={() => handleSort('asc', 'name')}><i className="fa-solid fa-arrow-down-a-z"></i></Dropdown.Item>
+                                                            <Dropdown.Item href="#/action-2" className="!rounded-lg" onClick={() => handleSort('dsc', 'name')}><i className="fa-solid fa-arrow-up-z-a"></i></Dropdown.Item>
+                                                        </DropdownButton>
+                                                    </th>
+                                                    <th className="border px-4 py-2">Age</th>
+                                                    <th className="border px-4 py-2">
+                                                        <DropdownButton id="dropdown-basic-button" title={<span className="font-bold">Department <i className="fa-solid fa-arrows-up-down ps-2"></i></span>} className="p-0 m-0">
+                                                            <Dropdown.Item href="#/action-1" className="text-center !rounded-lg" onClick={() => handleSort('asc', 'department')}><i className="fa-solid fa-arrow-down-a-z"></i></Dropdown.Item>
+                                                            <Dropdown.Item href="#/action-2" className="text-center !rounded-lg" onClick={() => handleSort('dsc', 'department')}><i className="fa-solid fa-arrow-up-z-a"></i></Dropdown.Item>
+                                                        </DropdownButton>
+                                                    </th>
+                                                    <th className="border px-4 py-2">
+                                                        <DropdownButton id="dropdown-basic-button" title={<span className="font-bold">Position <i className="fa-solid fa-arrows-up-down ps-2"></i></span>} className="p-0 m-0">
+                                                            <Dropdown.Item href="#/action-1" className="text-center !rounded-lg" onClick={() => handleSort('asc', 'position')}><i className="fa-solid fa-arrow-down-a-z"></i></Dropdown.Item>
+                                                            <Dropdown.Item href="#/action-2" className="text-center !rounded-lg" onClick={() => handleSort('dsc', 'position')}><i className="fa-solid fa-arrow-up-z-a"></i></Dropdown.Item>
+                                                        </DropdownButton>
+                                                    </th>
+                                                    <th className="border px-4 py-2">Salary</th>
+                                                    <th className="border px-4 py-2">
+                                                        <DropdownButton id="dropdown-basic-button" title={<span className="font-bold">Email <i className="fa-solid fa-arrows-up-down ps-2"></i></span>} className="p-0 m-0">
+                                                            <Dropdown.Item href="#/action-1" className="text-center !rounded-lg" onClick={() => handleSort('asc', 'email')}><i className="fa-solid fa-arrow-down-a-z"></i></Dropdown.Item>
+                                                            <Dropdown.Item href="#/action-2" className="text-center !rounded-lg" onClick={() => handleSort('dsc', 'email')}><i className="fa-solid fa-arrow-up-z-a"></i></Dropdown.Item>
+                                                        </DropdownButton>
+                                                    </th>
+                                                    <th className="border px-4 py-2 " style={{ width: '300px' }}>Action</th>
                                                 </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
-                                )}
+                                            </thead>
+                                            <tbody>
+                                                {filteredData.map((emp) => (
+                                                    <tr key={emp.id} className="hover:bg-gray-100 text-center">
+                                                        <td className="border px-4 py-3">{emp.id}</td>
+                                                        <td className="border px-4 py-3">{emp.name}</td>
+                                                        <td className="border px-4 py-3">{emp.age}</td>
+                                                        <td className="border px-4 py-3">{emp.department}</td>
+                                                        <td className="border px-4 py-3">{emp.position}</td>
+                                                        <td className="border px-4 py-3">{emp.salary}</td>
+                                                        <td className="border px-4 py-3">{emp.email}</td>
+                                                        <td className="border px-4 py-3">
+                                                            <button className="bg-green-700 p-2 px-3 rounded-lg me-2 text-white" onClick={() => empView(emp.id)}><i className="fa-solid fa-eye"></i></button>
+                                                            <button className="bg-blue-700 p-2 px-3 rounded-lg me-2 text-white" onClick={() => empEdit(emp.id)}><i className="fa-solid fa-pen-to-square"></i></button>
+                                                            <a href="#" className="bg-red-700 p-2 px-3 rounded-lg text-white" onClick={() => empDelete(emp.id)}><i className="fa-solid fa-trash"></i></a>
+                                                        </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    )}
+                                </div>
                             </div>
                         </div>
                     </Row>
